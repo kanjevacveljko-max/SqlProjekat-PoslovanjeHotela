9e78b79a9cb69b97115acbcc49b3ae0a05ed6bba
Veljko <kanjevacveljko@gmail.com>
2025-09-12 01:33:44 +0200

    Changed database objects 1-4

--- Kreiranje_objekata_baze.sql
+++ Kreiranje_objekata_baze.sql
@@ -1,160 +1,150 @@
 --1. Kreiranje pogleda view_PregledRezervacija koji prikazuje sve rezervacije i sve podatke o njima.
 
-CREATE OR ALTER VIEW dbo.view_PregledRezervacija
-AS
-SELECT
+create view dbo.view_PregledRezervacija
+as
+select
     r.id_rezervacije, 
-    g.id_gosta, g.ime AS ime_gosta, g.prezime AS prezime_gosta, g.telefon AS telefon_gosta, 
-    g.email AS email_gosta, s.id_sobe, s.broj_sobe, s.sprat, s.tip_kreveta, s.osnovna_cena,
-    z.id_zaposlenog, z.ime AS ime_zaposlenog, z.prezime AS prezime_zaposlenog,
+    g.id_gosta, g.ime as ime_gosta, g.prezime as prezime_gosta, g.telefon as telefon_gosta, 
+    g.email as email_gosta, s.id_sobe, s.broj_sobe, s.sprat, s.tip_kreveta, s.osnovna_cena,
+    z.id_zaposlenog, z.ime as ime_zaposlenog, z.prezime as prezime_zaposlenog,
     r.datum_prijave, r.datum_odjave, r.broj_nocenja, r.broj_gostiju, r.status, r.datum_kreiranja,
-    r.broj_nocenja * s.osnovna_cena AS cena_sobe
-FROM dbo.Rezervacije r
-JOIN dbo.Gosti g ON g.id_gosta = r.id_gosta
-JOIN dbo.Sobe s ON s.id_sobe = r.id_sobe
-LEFT JOIN dbo.Zaposleni z ON z.id_zaposlenog = r.id_zaposlenog;
-GO
+    r.broj_nocenja * s.osnovna_cena as cena_sobe
+from dbo.Rezervacije r
+join dbo.Gosti g on g.id_gosta = r.id_gosta
+join dbo.Sobe s on s.id_sobe = r.id_sobe
+left join dbo.Zaposleni z on z.id_zaposlenog = r.id_zaposlenog;
+go
 
 
 --2. Kreiranje pogleda view_SobeNaRapolaganju koji prikazuje sve sobe koje su trenutno na raspolaganju sa
 --   podacima o njima
 
-CREATE OR ALTER VIEW dbo.v_SobeNaRaspolaganju
-AS
+create view dbo.view_SobeNaRaspolaganju
+as
 
-WITH Aktivne AS (
-    SELECT r.id_sobe
-    FROM dbo.Rezervacije r
-    WHERE r.status IN (N'rezervisano', N'prijavljen')
-      AND CAST(GETDATE() AS date) >= r.datum_prijave
-      AND CAST(GETDATE() AS date) <  r.datum_odjave
-    GROUP BY r.id_sobe
+with Aktivne as (
+    select r.id_sobe
+    from dbo.Rezervacije r
+    where r.status in (N'rezervisano', N'prijavljen')
+      and cast(getdate() as date) >= r.datum_prijave
+      AND cast(getdate() as date) <  r.datum_odjave
+    group by r.id_sobe
 )
-SELECT 
+select 
     s.id_sobe, s.broj_sobe, s.sprat, s.tip_kreveta,
     s.osnovna_cena, s.status
-FROM dbo.Sobe s
-LEFT JOIN Aktivne a ON a.id_sobe = s.id_sobe
-WHERE a.id_sobe IS NULL
-      AND (s.status IS NULL OR s.status NOT IN 
+from dbo.Sobe s left join Aktivne a 
+     on a.id_sobe = s.id_sobe
+where a.id_sobe is null
+      and (s.status is null or s.status not in
       (N'zauzeta', N'van upotrebe'));
-GO
+go
 
 
 -- 3. Kreinran funkije fn_TrenutniTrosakSobe koja nam prikazuje trenutno zaduzenje za sobu ciji smo id
 --    prosledili sa uracunatim dodantnim uslugama.
 
-CREATE OR ALTER FUNCTION dbo.fn_TrenutniTrosakSobe
-(
-    @id_sobe INT
-)
-RETURNS DECIMAL(18,2)
-AS
-BEGIN
-    DECLARE 
-        @id_rezervacije INT,
-        @osnovna_cena  DECIMAL(10,2),
-        @datum_prijave DATE,
-        @datum_odjave  DATE,
-        @nocenja_do_danas INT,
-        @iznos_soba    DECIMAL(10,2),
-        @iznos_usluge  DECIMAL(10,2),
-        @rezultat      DECIMAL(10,2);
+alter function fn_TrenutniTrosakSobe(@idSobe int)
+returns real
+as
+begin
+    declare @danasnji_datum date = cast(getdate() as date),
+            @id_rezervacije int,
+            @datum_prijave date,
+            @datum_odjave date,
+            @nocenja_do_danas int,
+            @osnovna_cena real,
+            @iznos_soba real,
+            @iznos_usluge real,
+            @rezultat real;
 
-    SELECT TOP 1
+    select top 1
         @id_rezervacije = r.id_rezervacije,
-        @osnovna_cena   = s.osnovna_cena,
-        @datum_prijave  = r.datum_prijave,
-        @datum_odjave   = r.datum_odjave
-    FROM dbo.Rezervacije r
-    JOIN dbo.Sobe s ON s.id_sobe = r.id_sobe
-    WHERE r.id_sobe = @id_sobe
-      AND r.status IN (N'rezervisano', N'prijavljen')
-      AND CAST(GETDATE() AS DATE) >= r.datum_prijave
-      AND CAST(GETDATE() AS DATE) <  r.datum_odjave
-    ORDER BY r.datum_prijave DESC;
+        @datum_prijave = r.datum_prijave,
+        @datum_odjave = r.datum_odjave,
+        @osnovna_cena = s.osnovna_cena
+    from rezervacije r inner join sobe s 
+         on r.id_sobe = r.id_sobe
+    where r.id_sobe = @idSobe and
+          @danasnji_datum > @datum_prijave and
+          @danasnji_datum < @datum_odjave and
+          r.status in (N'prijavljen', N'rezervisano')
+    order by r.datum_prijave desc
 
-    IF @id_rezervacije IS NULL
-        RETURN NULL;
+    if @id_rezervacije is null
+        return null;
 
-    SET @nocenja_do_danas = DATEDIFF(DAY, @datum_prijave, CAST(GETDATE() AS DATE));
-    IF @nocenja_do_danas < 1 SET @nocenja_do_danas = 1;
+    set @nocenja_do_danas = datediff(day, @datum_prijave, @danasnji_datum);
+    if @nocenja_do_danas < 1
+        set @nocenja_do_danas = 1;
 
-    SET @iznos_soba = @osnovna_cena * @nocenja_do_danas;
+    set @iznos_soba = @nocenja_do_danas * @osnovna_cena;
 
-    SELECT @iznos_usluge = ISNULL(SUM(kolicina * jedinicna_cena), 0)
-    FROM dbo.Usluge
-    WHERE id_rezervacije = @id_rezervacije
-          AND datum_usluge <= CAST(GETDATE() AS DATE);
+    select @iznos_usluge = isnull(sum(kolicina * jedinicna_cena), 0)
+    from usluge
+    where id_rezervacije = @id_rezervacije and
+          datum_usluge <= @danasnji_datum;
 
-    SET @rezultat = @iznos_soba + ISNULL(@iznos_usluge, 0);
+    set @rezultat = @iznos_soba + isnull(@iznos_usluge, 0);
 
-    RETURN @rezultat;
-END
-GO
+    return @rezultat;
 
-SELECT dbo.fn_TrenutniTrosakSobe(5) AS trosak_do_danas;
+end
+go
 
 
 --4. Kreiranje inline table-value funkcije fn_RacunRezime koja nam vraca racun po stavkama za rezervaciju ciji smo
 --   id prosledili funkciji.
 
-CREATE FUNCTION dbo.fn_RacunRezime
-(
-    @id_rezervacije INT
-)
-RETURNS @racun TABLE
-(
-    id_rezervacije INT,
-    iznos_soba DECIMAL(18,2),
-    iznos_usluga DECIMAL(18,2),
-    ukupno_placeno DECIMAL(18,2),
-    ukupno_zaduzenje DECIMAL(18,2),
-    saldo DECIMAL(18,2)
-)
-AS
-BEGIN
-    DECLARE
-        @cena_noc DECIMAL(18,2),
-        @br_noc INT,
-        @soba DECIMAL(18,2),
-        @usluge DECIMAL(18,2),
-        @placeno DECIMAL(18,2);
+create function fn_RacunRezime(@idRezervacije int)
+returns @Rezultat table(
+        id_rezervacije int,
+        iznos_soba real,
+        iznos_usluge real,
+        ukupno_zaduzenje real,
+        ukupno_placeno real,
+        saldo real)
+as
+begin
+    declare @broj_nocenja int,
+            @cena_noc real,
+            @soba real,
+            @usluge real,
+            @placeno real;
 
-    SELECT 
-        @cena_noc = s.osnovna_cena,
-        @br_noc   = r.broj_nocenja
-    FROM dbo.Rezervacije r
-    JOIN dbo.Sobe s ON s.id_sobe = r.id_sobe
-    WHERE r.id_rezervacije = @id_rezervacije;
+    select 
+        @broj_nocenja = r.broj_nocenja,
+        @cena_noc = s.osnovna_cena
+    from rezervacije r join sobe s 
+         on r.id_sobe = s.id_sobe
+    where r.id_rezervacije = @idRezervacije;
 
-    IF @cena_noc IS NULL OR @br_noc IS NULL
-        RETURN;
+    if @cena_noc is null or @broj_nocenja is null 
+        return;
 
-    SET @soba = @cena_noc * @br_noc;
+    set @soba = @cena_noc * @broj_nocenja;
 
-    SELECT @usluge = ISNULL(SUM(kolicina * jedinicna_cena), 0)
-    FROM dbo.Usluge
-    WHERE id_rezervacije = @id_rezervacije;
+    select @usluge = isnull(sum(kolicina * jedinicna_cena), 0)
+    from usluge
+    where id_rezervacije = @idRezervacije;
 
-    SELECT @placeno = ISNULL(SUM(iznos), 0)
-    FROM dbo.Placanja
-    WHERE id_rezervacije = @id_rezervacije;
+    select @placeno = isnull(sum(iznos), 0)
+    from placanja
+    where id_rezervacije = @idRezervacije;
 
-    INSERT INTO @racun
-    VALUES
-    (
-        @id_rezervacije, @soba, @usluge, @placeno,
-        @soba + @usluge, (@soba + @usluge) - @placeno
-    );
+    insert into @Rezultat (id_rezervacije, iznos_soba, iznos_usluge, ukupno_zaduzenje,
+                          ukupno_placeno, saldo)
+    values (@idRezervacije, @soba, @usluge, @soba+@usluge, @placeno, @soba+@usluge-@placeno);
 
-    RETURN;
-END
-GO
+    return;
+end
+go
+
+        
 
 
 -- 5. Kreiranje multistatement table-value funkcije koja vraca sve prethodne rezervacije gosta ciji
---    id_prosledimo.
+--    id prosledimo.
 
 CREATE FUNCTION dbo.fn_RezervacijeZaGosta
 (
